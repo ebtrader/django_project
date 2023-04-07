a6a006eb2bf7175ebcde040eb09d261d16151f4d
ebtrader <jsiddique@gmail.com>
2023-04-07 19:49:57 -0400

    Initial commit

---
+++ passenger_wsgi.py
@@ -0,0 +1,21 @@
+import sys, os
+
+INTERP = os.path.join(os.environ['HOME'], '', 'venv', 'bin', 'python3')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+cwd = os.getcwd()
+sys.path.append(os.getcwd())
+
+# INTERP = "/home/lemonaid/dj/bin/python3" # absolute path to your virtual env
+# #INTERP is present twice so that the new python interpreter
+# #knows the actual executable path
+# if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+# cwd = os.getcwd()
+# sys.path.append(cwd)
+sys.path.append(cwd + '/helloworld')  # name of your Django project
+sys.path.insert(0,cwd+'../venv/bin') # virtual env # relative path to your virtual env
+sys.path.insert(0,cwd+'../venv/lib/python3.8/site-packages') # relative path to your virtual env
+os.environ['DJANGO_SETTINGS_MODULE'] = "helloworld.settings" # name of your Django project
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
